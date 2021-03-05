48f5b2eee52812d192827576a2b75afd10e3de67
ganushkevychv <ganushkevychv@gmail.com>
2021-03-05 15:57:23 +0100

    components folder created, Header created as separated component

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Header = () => {
+
+return(
+
+<header className="App-header">
+        <h1>@nastkazgdanska</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+)
+}
+export default Header;
